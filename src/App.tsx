42f3a3b43337f3bdec6e7d236535c518121f5fe9
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-05-02 12:50:10 +0300

    create Todolist create PropsType and TaskType App const tasks1: Array<TaskType> законектил типы

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TaskType, Todolist } from './Components/DodoList/Todolist';
+
+const tasks1: Array<TaskType> = [
+	{ id: 1, title: 'html/CSS', isDone: true },
+	{ id: 2, title: 'js', isDone: true },
+	{ id: 3, title: 'react', isDone: true }
+]
+const tasks2: Array<TaskType> = [
+	{ id: 1, title: 'lkhlkh', isDone: true },
+	{ id: 2, title: 'lknlk', isDone: true },
+	{ id: 3, title: 'lkhlkh', isDone: true }
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Todolist title={'What to learn'} tasks={tasks1} />
+			<Todolist title={'Movies'} tasks={tasks2} />
+			{/*<Todolist title={'Songs'} />*/}
+		</div>
+	);
 }
 
 export default App;
