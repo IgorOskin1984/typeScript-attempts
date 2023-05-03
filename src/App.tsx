2018780061ec11caee915660c9d771628c93dbe6
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-05-03 14:48:25 +0300

    feat: pass props add PropsType and TaskType create listItemCreater

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Todolist } from './Components/Todolist/Todolist';
+
+const title = 'What to learn'
+
+const tasks = [
+	{ id: 1, title: 'Rest API', isDone: true },
+	{ id: 2, title: 'HTML CSS', isDone: false },
+	{ id: 3, title: 'React', isDone: false },
+	{ id: 4, title: 'Redux', isDone: true },
+]
 
 function App() {
 	return (
 		<div className="App">
-			<Todolist />
+			<Todolist title={title}
+				tasks={tasks}
+			/>
 		</div>
 	);
 }
--- src/Components/Todolist/Todolist.tsx
+++ src/Components/Todolist/Todolist.tsx
@@ -1,40 +1,46 @@
 import React from "react";
 import s from './Todolist.module.css'
 
-export const Todolist = () => {
+export type TaskType = {
+	id: number
+	title: string
+	isDone: boolean
+}
+
+type PropsType = {
+	title: string
+	tasks: Array<TaskType>
+}
+
+export const Todolist = (props: PropsType) => {
+
+	const listItemCreater = (tasks: Array<TaskType>) => {
+		return tasks.map((t) => {
+			return (
+				<li key={t.id} className={s.item}>
+					<input type="checkbox"
+						checked={t.isDone} />
+					<span>{t.title}</span>
+					<button>X</button>
+				</li>
+			)
+		})
+	}
 	return (
 		<div className={s.container}>
-			<h2>Hello</h2>
-			<div className={s.inputContaiher}>
+			<h2>{props.title}</h2>
+
+			<div className={s.inputContainer}>
 				<input type="text"
 					placeholder="Type new task"
 				/>
 				<button>+</button>
 			</div>
-			<ul className={s.list}>
-				<li className={s.item}>
-					<input type="checkbox"
-						checked={true} />
-					<span>Rest API</span>
-					<button>X</button>
-				</li>
-				<li className={s.item}>
-					<input type="checkbox" />
-					<span>Rest API</span>
-					<button>X</button>
-				</li>
-				<li className={s.item}>
-					<input type="checkbox" />
-					<span>Rest API</span>
-					<button>X</button>
-				</li>
-				<li className={s.item}>
-					<input type="checkbox" />
-					<span>Rest API</span>
-					<button>X</button>
-				</li>
 
+			<ul className={s.list}>
+				{listItemCreater(props.tasks)}
 			</ul >
+
 			<div className={s.actives}>
 				<button>All</button>
 				<button>Active</button>
