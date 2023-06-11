5ed602034e73790298e952515df8196189f83b71
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-11 19:29:35 +0300

    onChangeTitleHandler

--- src/Components/TodoList/EditeblSpan.tsx
+++ src/Components/TodoList/EditeblSpan.tsx
@@ -1,22 +1,26 @@
-import { useState } from "react"
+import { ChangeEvent, useState } from "react"
 
 type EditeblSpanPropsType = {
 	title: string
 }
 export const EditeblSpan = (props: EditeblSpanPropsType) => {
 	const [editMode, seteditMode] = useState(false)
+	const [title, setTitle] = useState('')
 
 	const activateEditMode = () => {
 		seteditMode(true)
+		setTitle(props.title)
 	}
 	const activateViewMode = () => {
 		seteditMode(false)
 	}
 
-
+	const onChangeTitleHandler = (e: ChangeEvent<HTMLInputElement>) => {
+		setTitle(e.currentTarget.value)
+	}
 
 	return editMode
-		? <input onBlur={activateViewMode} type="text" value={props.title} autoFocus />
+		? <input onChange={onChangeTitleHandler} onBlur={activateViewMode} type="text" value={title} autoFocus />
 		: <span onDoubleClick={activateEditMode}>{props.title}</span>
 
 }
