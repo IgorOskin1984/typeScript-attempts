1b09dfa757f84056a0000dacbe3ab19250ccaa87
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-11 18:51:19 +0300

    Create activateEditMode

--- src/Components/TodoList/EditeblSpan.tsx
+++ src/Components/TodoList/EditeblSpan.tsx
@@ -1,6 +1,17 @@
+import { useState } from "react"
+
 type EditeblSpanPropsType = {
 	title: string
 }
 export const EditeblSpan = (props: EditeblSpanPropsType) => {
-	return <span>{props.title}</span>
+	const [editMode, seteditMode] = useState(false)
+
+	const activateEditMode = () => {
+		seteditMode(true)
+	}
+
+	return editMode
+		? <input type="text" value={props.title} />
+		: <span onDoubleClick={activateEditMode}>{props.title}</span>
+
 }
