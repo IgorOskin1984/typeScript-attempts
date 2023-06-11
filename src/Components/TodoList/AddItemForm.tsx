1a3fed4b55705afb7bb9e89d59131ed3ef64f61f
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-11 14:10:18 +0300

    Создал AddItemForm И перенес туда инпут, button и логику

---
+++ src/Components/TodoList/AddItemForm.tsx
@@ -0,0 +1,55 @@
+import { ChangeEvent, KeyboardEvent, useState } from "react"
+import s from './Todolist.module.css'
+
+type AddItemFormPropsTpe = {
+	addTask: (value: string, todolistsId: string) => void
+	todolistId: string
+}
+
+export const AddItemForm = (props: AddItemFormPropsTpe) => {
+	const [newTaskTitle, setNewTaskTitle] = useState('')
+	const [error, setError] = useState<string | null>(null)
+
+	const onNewTaskTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setNewTaskTitle(e.currentTarget.value)
+		setError('')
+	}
+
+	const onKeyPressUpHendler = (e: KeyboardEvent<HTMLInputElement>) => {
+		setError(null)
+		if (e.code === 'Enter') {
+			addTask()
+		}
+	}
+
+	const addTask = () => {
+		if (newTaskTitle.trim() !== '') {
+			props.addTask(newTaskTitle.trim(), props.todolistId)
+			setNewTaskTitle('')
+		}
+		else {
+			setError('Empty field')
+		}
+	}
+
+
+	return <>
+		<div className={s.wrapper}>
+			<div className={s.inputContainer} >
+				<input placeholder={'type new task'}
+					value={newTaskTitle}
+					onChange={onNewTaskTitleChange}
+					onKeyUp={onKeyPressUpHendler}
+					onBlur={() => { setError('') }}
+					className={error ? s.error : ''}
+				/>
+				<button onClick={addTask} >+</button>
+			</div>
+			{error &&
+				<div className={s.errorMessageDiv}>
+					<p>{error}</p>
+				</div>
+			}
+		</div>
+	</>
+}
