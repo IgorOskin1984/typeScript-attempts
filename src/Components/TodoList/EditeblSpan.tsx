969a806a30f4b6019f5bb6bd9ef4ba41d4830517
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-12 10:31:39 +0300

    onChangeHandler

--- src/Components/TodoList/EditeblSpan.tsx
+++ src/Components/TodoList/EditeblSpan.tsx
@@ -5,18 +5,28 @@ type EditeblSpanPropsType = {
 }
 export const EditeblSpan = (props: EditeblSpanPropsType) => {
 	const [editMode, seteditMode] = useState(false)
+	const [title, setTitle] = useState('')
 
 	const activateEditMode = () => {
 		seteditMode(true)
+		setTitle(props.title)
 	}
 	const activateViewMode = () => {
 		seteditMode(false)
+		setTitle('')
 	}
 
 	const onKeyUpHandler = (e: KeyboardEvent<HTMLInputElement>) => {
 		if (e.key === 'Enter') activateViewMode()
 	}
+
+	const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+		setTitle(e.currentTarget.value)
+	}
+
 	return editMode
-		? <input onKeyUp={onKeyUpHandler} onBlur={activateViewMode} autoFocus type="text" value={props.title} />
+		? <input onChange={onChangeHandler}
+			onKeyUp={onKeyUpHandler} onBlur={activateViewMode} autoFocus type="text"
+			value={title} />
 		: <span onDoubleClick={activateEditMode}>{props.title} </span>
 }
